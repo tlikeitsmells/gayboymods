f56b387516fc714ee35b0d346ec8c941f5c02930
ryan-hp <atxxx@duck.com>
2025-03-20 09:13:54 -0500

    zips to front

--- plugin.video.xnxx/main.py
+++ plugin.video.xnxx/main.py
@@ -3,113 +3,47 @@
 # Author: moedje (Roman V. M. SimplePlugin/example framework)
 # Created on: 27.08.2017
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
-import sys, json, os, re #, simpleutils
+import sys, json, os, re, simpleutils
 import xbmc, xbmcgui
-import urlquick
+from urllib import quote_plus
 from simpleplugin import Plugin, Params
-try:
-    from urllib import quote_plus, urlencode
-except:
-    from urllib import parse
-    quote_plus = parse.quote_plus
-try:
-    from operator import itemgetter
-except:
-    import operator
-    itemgetter = operator.itemgetter
+from operator import itemgetter
 
-plugin = Plugin()   # Get the plugin url in plugin:// notation. plugin
+plugin = Plugin()   # Get the plugin url in plugin:// notation.
 _url = sys.argv[0]  # Get the plugin handle as an integer number.
 _handle = int(sys.argv[1])
 __addondir__ = xbmc.translatePath(plugin.addon.getAddonInfo('path'))
 __resources__ = os.path.join(__addondir__, 'resources/')
 __next__ = os.path.join(__resources__, "next.png")
-__filtersex__ = 'gay'
-__sortby__ = 'hits'
-__period__ = 'ever'
-
+__filtersex__ = plugin.get_setting('filterorientation')
+__sortby__ = plugin.get_setting('sortby')
 
 VIDEOS = json.load(file(os.path.join(__resources__, 'tags.json')))
-webreq = urlquick.get # simpleutils.CachedWebRequest(cookiePath=os.path.join(xbmc.translatePath('special://profile'), 'addon_data/', plugin.id))
+webreq = simpleutils.CachedWebRequest(cookiePath=os.path.join(xbmc.translatePath('special://profile'), 'addon_data/', plugin.id))
 revidblock = re.compile(ur'<div id="(video_.*?)</p></div>', re.DOTALL)
-reGetvids = re.compile(ur'href="(.+?)".*?data-src="(.+?)".*?title="(.+?)"')
+revidparts = re.compile(ur'video_([\d]+).*? src="(.*?)".*?href="(.*?)".*?itle="(.*?)".*?"duration">[\(](.+?)[\)]</span',re.DOTALL)
+
 
 def parsepageforvideos(html):
-    matches = []
-    vmatches = []
-    vidhtml = ""
     matches = revidblock.findall(html)
     listitems = []
     for vidhtml in matches:
-        vmatches = reGetvids.findall(str(vidhtml))
+        vmatches = revidparts.findall(vidhtml)
         if vmatches is not None:
-            for link, img, vname in vmatches:
-                lenpart = ""
-                hits = ""
-                rating = ""
-                vname = vname.title().strip()
-                title = vname.replace('.', '').replace(',','').replace("'","")
-                if len(title) > 60:
-                    firstwords = title.split(' ')
-                    word1 = firstwords[0]
-                    word2 = ""
-                    if len(firstwords) > 2:
-                        word2 = firstwords[1] + " " + firstwords[2]
-                    elif len(firstwords) > 1:
-                        word2 = firstwords[1]
-                    title = "{0} {1}".format(word1, word2)
-                    endlen = (60 - len(title))
-                    tempname = vname[endlen:]
-                    title = title + ".." + tempname
-                try:
-                    hits = vidhtml.split('"metadata"><span class="right">\n', 1)[-1].split(' ', 1)[0].strip()
-                except:
-                    pass
-                try:
-                    rating = vidhtml.split('class="superfluous">', 1)[-1].split('</span>', 1)[0].strip()
-                    if len(rating) > 3:
-                        rating = rating[0:2]
-                    elif rating == '-':
-                        rating = ""
-                    if rating.find("%") == -1:
-                        rating = ""
-                except:
-                    rating = ""
-                if vidhtml.find('sec') == -1 and vidhtml.find('min') == -1:
-                    length = ""
-                else:
-                    length = vidhtml.split("</span></span>\n",1 )[-1].split('\n',1)[0].strip()
-                    if len(length) > 12:
-                        length = length[0:11]
-                        lparts = length.split(' ')
-                        length = lparts[0]
-                        if len(lparts) > 1:
-                            secs = lparts[1]
-                            if secs.find('sec') != -1:
-                                length += " " + secs
-                link = "https://www.xnxx.com" + link
-                itempath = plugin.get_url(action='play', url=link)
-
-                if len(rating) > 0:
-                    rating = "{0} Rating".format(rating)
-                if len(hits) > 0:
-                    hits = "{0} Views".format(hits)
-                label = "[B][COLOR white]{0}[/COLOR][/B]\n".format(title)
-                label2 = "[COLOR yellow]{0}[/COLOR] [I][COLOR orange][B]{1}[/B] [/COLOR] [COLOR red]{2}[/COLOR][/I]".format(length, hits, rating)
-                label = label + label2.center(80)
-
-                imgpart = img.rpartition('/')
-                fanart = "{0}{1}mozaiquefull.jpg".format(imgpart[0], imgpart[1])
+            for vidid, img, link, title, length in vmatches:
+                linkfull = 'https://flashservice.xvideos.com/embedframe/' + vidid
+                label = title + " [COLOR white][I][B]({0})[/B][/I][/COLOR]".format(length)
+                label2 = str(length)
+                itempath = plugin.get_url(action='play', url=linkfull)
                 img = img.replace('THUMBNUM', '{0}')
                 icon = img.format(5)
-                clearart = img.format(10)
-                thumb = img.format(15)
-                poster = thumb = img.format(20)
-
-                artwork = {'poster': poster, 'clearart': clearart, 'thumb': thumb, 'icon': icon, 'fanart': fanart}
-                mitem = {'label': label, 'label2': label2, 'icon': icon, 'thumb': thumb, 'fanart': fanart, 'url': itempath,
-                         'is_playable': True, 'is_folder': False, 'art': artwork}
-                mitem.update(ctx(link))
+                thumb = img.format(25)
+                fanart = img.format(10)
+                clearart = img.format(15)
+                artwork = {'clearart': clearart, 'thumb': thumb, 'icon': icon, 'fanart': fanart, 'thumb': thumb}
+                mitem = {'label': label, 'label2': label2, 'icon': icon, 'thumb': thumb, 'url': itempath,
+                         'is_playable': True, 'is_folder': False, 'art': artwork, 'mime': 'video/mp4', 'info': {'video': {'Runtime': length}}}
+                mitem.update(ctx(linkfull))
                 listitems.append(mitem)
     return listitems
 
@@ -117,12 +51,12 @@ def DL(url):
     resp = None
     html = None
     try:
-        resp = webreq(url)
-        html = resp.read() #.encode('latin-1', 'ignore')
+        resp = webreq.getSource(url)
+        html = resp.encode('latin-1', 'ignore')
     except:
         try:
-            resp = webreq(url)
-            html = resp.read().encode('latin-1', 'ignore')
+            resp = webreq.getSource(url)
+            html = simpleutils.try_coerce_native(resp)
         except:
             html = resp
     return html
@@ -134,14 +68,13 @@ def setView(id=500):
         pass
 
 def ctx(url):
+    #pathdl = "plugin://plugin.video.tumblrv/download/" + vidurl
     pathdl = plugin.get_url(action='download', vidurl=url)
     citem = ('Download', 'RunPlugin({0})'.format(pathdl),)
     return {'context_menu': [citem]}
 
-
 def notify(msg):
     xbmc.executebuiltin("Notification('{0}')".format(msg))
-
 
 def getinput(title='Search', default=None):
     search_term = None
@@ -159,7 +92,6 @@ def getinput(title='Search', default=None):
     else:
         search_term = False
     return search_term
-
 
 @plugin.action()
 def root():
@@ -199,7 +131,6 @@ def taglistforletter(params):
     return listitems
     #return plugin.create_listing(listitems, succeeded=True, update_listing=True, cache_to_disk=False, view_mode=51, content='movies')
 
-
 @plugin.action()
 def videosfortag(params):
     tagname = params.tagname
@@ -207,14 +138,13 @@ def videosfortag(params):
     doup = False
     if params.page is not None:
         pagenum = params.page
-    return dosearch(page=pagenum, term=tagname)
+    tagurl = "http://www.xnxx.com/tags/{0}/{1}/t:{2}/s:{3}".format(tagname, str(pagenum), __filtersex__, __sortby__)
+    pagenum = str(1 + int(pagenum))
+    nexturl = "http://www.xnxx.com/tags/{0}/{1}/t:{2}/s:{3}".format(tagname, str(pagenum), __filtersex__, __sortby__)
+    nextpagepath = plugin.get_url(action='videosfortag', tagname=tagname, page=pagenum)
+    nextitem = {'label': 'Next -> #{0}'.format(pagenum), 'label2': nexturl, 'thumb': os.path.join(__resources__, 'next.png'), 'url': nextpagepath}
+    html = DL(tagurl)
     '''
-    #tagurl = "http://www.xnxx.com/tags/{0}/{1}/t:{2}/s:{3}".format(tagname, str(pagenum), __filtersex__, __sortby__)
-    #pagenum = str(1 + int(pagenum))
-    #nexturl = "http://www.xnxx.com/tags/{0}/{1}/t:{2}/s:{3}".format(tagname, str(pagenum), __filtersex__, __sortby__)
-    #nextpagepath = plugin.get_url(action='videosfortag', tagname=tagname, page=pagenum)
-    #nextitem = {'label': 'Next -> #{0}'.format(pagenum), 'label2': nexturl, 'thumb': os.path.join(__resources__, 'next.png'), 'url': nextpagepath}
-    #html = DL(tagurl)
     try:
         resp = webreq.getSource(url=tagurl).encode('latin-1', 'ignore')
         html = resp.partition('div class="mozaique"')[-1].rpartition('class="no-page">Next</a>')[0]
@@ -225,12 +155,14 @@ def videosfortag(params):
         html = simpleutils.try_coerce_native(resp).partition('div class="mozaique"')[-1].rpartition('class="no-page">Next</a>')[0]
     except:
         plugin.log('Error downloading page', xbmc.LOGERROR)
+    '''
     matches = revidblock.findall(html)
     listitems = []
     listitems = parsepageforvideos(html)
     if len(listitems) > 0:
       listitems.append(nextitem)
     return listitems
+    '''
     for vidhtml in matches:
         vmatches = revidparts.findall(vidhtml)
         if vmatches is not None:
@@ -269,21 +201,15 @@ def download(params):
             notify("No video URL was found to download")
     return None
 
-
 @plugin.action()
 def newsearch():
     term = getinput()
     if not term:
         notify('No search terms provided')
         return None
-    #return search(params=urlencode({'page': 0, 'term': quote_plus(term)}))
-    return dosearch(page=0, term=term)
-    '''
     nextpathurl = plugin.get_url(action='search', page=0, term=term)
-    surl = "http://www.xnxx.com/search/{0}/{1}/{2}/{3}/" .format(__sortby__, __period__, __filtersex__, term, 0)
-    nexturl = "http://www.xnxx.com/search/{0}/{1}/{2}/{3}/".format(__sortby__, __period__, __filtersex__, term, 1)
-    #surl = "http://www.xnxx.com/?k={0}&p={1}&typef={2}&sort={3}&datef=all&durf=all".format(term, 0, __filtersex__,__sortby__)
-    #nexturl = "http://www.xnxx.com/?k={0}&p={1}&typef={2}&sort={3}&datef=all&durf=1-10min".format(term, 1,__filtersex__,__sortby__)
+    surl = "http://www.xnxx.com/?k={0}&p={1}&typef={2}&sort={3}&datef=all&durf=all".format(term, 0, __filtersex__,__sortby__)
+    nexturl = "http://www.xnxx.com/?k={0}&p={1}&typef={2}&sort={3}&datef=all&durf=1-10min".format(term, 1,__filtersex__,__sortby__)
     nextpathurl = plugin.get_url(action='search', page=1, term=term)
     nextlbl = "[B]Next[/B] -> Page [COLOR green]#{0}[/COLOR]".format(2)
     nextitem = {'label': nextlbl, 'label2': "1", 'thumb': __next__, 'url': nextpathurl}
@@ -293,8 +219,6 @@ def newsearch():
     if len(listitems) > 0:
         listitems.append(nextitem)
     return listitems
-    '''
-
 
 @plugin.action()
 def search(params):
@@ -302,29 +226,21 @@ def search(params):
     term = params.term
     if page is None:
         page = 0
+    nextpage = int(page) + 1
     if term is None:
         return None
-    return dosearch(page,term)
-
-
-def dosearch(page=0, term=""):
-    nextpage = int(page) + 1
     term = quote_plus(term)
-    surl = "http://www.xnxx.com/search/{0}/{1}/{2}/{3}/{4}/".format(__sortby__, __period__, __filtersex__, term, str(page))
-    #xbmc.log("Search URL: " + surl)
-    #nexturl = "http://www.xnxx.com/search/{0}/{1}/{2}/{3}/{4}/".format(__sortby__, __period__, __filtersex__, term, nextpage)
-    #surl = "http://www.xnxx.com/?k={0}&p={1}&typef={2}&sort={3}&datef=all&durf=all".format(term, page, __filtersex__, __sortby__)
-    #nexturl = "http://www.xnxx.com/?k={0}&p={1}&typef={2}&sort={3}&datef=all&durf=1-10min".format(term, nextpage, __filtersex__,  __sortby__)
+    surl = "http://www.xnxx.com/?k={0}&p={1}&typef={2}&sort={3}&datef=all&durf=all".format(term, page, __filtersex__, __sortby__)
+    nexturl = "http://www.xnxx.com/?k={0}&p={1}&typef={2}&sort={3}&datef=all&durf=1-10min".format(term, nextpage, __filtersex__,  __sortby__)
     nextpathurl = plugin.get_url(action='search', page=nextpage, term=term)
-    nextlbl = "[B]Next[/B] -> Page [COLOR green]#{0}[/COLOR]".format(str(int(nextpage)+1))
+    nextlbl = "[B]Next[/B] -> Page [COLOR green]#{0}[/COLOR]".format(nextpage)
     nextitem = {'label': nextlbl, 'label2': str(nextpage), 'thumb': __next__, 'url': nextpathurl}
     html = DL(surl)
     listitems = []
     listitems = parsepageforvideos(html)
-    if len(listitems) > 0 and len(listitems) > 34:
+    if len(listitems) > 0:
         listitems.append(nextitem)
     return listitems
-
 
 @plugin.action()
 def play(params):
@@ -332,10 +248,8 @@ def play(params):
     # Return a string containing a playable video URL
     plugin.log(message=str("** play: " + str(repr(params))), level=xbmc.LOGINFO)
     vidurl = params.url
-    #webreq = simpleutils.CachedWebRequest(cookiePath=os.path.join(xbmc.translatePath('special://profile'), 'addon_data/', plugin.id))
-    #resp = simpleutils.to_unicode(webreq.getSource(vidurl))
-    resp = DL(vidurl)
-    # html5player.setVideoUrlHigh('
+    webreq = simpleutils.CachedWebRequest(cookiePath=os.path.join(xbmc.translatePath('special://profile'), 'addon_data/', plugin.id))
+    resp = simpleutils.to_unicode(webreq.getSource(vidurl))
     movurl = resp.split("html5player.setVideoUrlHigh('",1)[-1].split("'",1)[0]
     if not movurl.startswith('http'):
         matches = re.compile(ur"html5player.setVideoUrlHigh\('(.*?)'").findall(resp)
@@ -348,9 +262,8 @@ def play(params):
     litem = plugin.create_list_item({"url": movurl})
     litem.setInfo(type='video', infoLabels={"Genre": "porn"})
     litem.setMimeType("video/mp4")
-    plugin.resolve_url(movurl)
-    #return plugin.create_listing(listing=plugin.resolve_url(path=movurl, play_item=litem, succeeded=True), update_listing=False)
-    return movurl
+    return plugin.create_listing(listing=plugin.resolve_url(path=movurl, play_item=litem, succeeded=True), update_listing=False)
+    #return movurl
 
 '''
 def get_categories(ITEMS):
@@ -489,7 +402,4 @@ def list_videos(category, **kwargs):
 '''
 
 if __name__ == '__main__':
-    __filtersex__ = plugin.get_setting('filterorientation')
-    __sortby__ = plugin.get_setting('sortby')
-    __period__ = plugin.get_setting('period')
     plugin.run()  # Start plugin
